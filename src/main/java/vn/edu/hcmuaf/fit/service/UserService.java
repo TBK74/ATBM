7f2c5f33f2762845a3805a86f0c968af6dc8bc0c
nam102nkt <Nam102nkt@gmail.com>
2026-04-17 23:14:42 +0700

    fix(User): sửa lỗi mã hóa mật khẩu khi đăng ký tài khoản

--- src/main/java/vn/edu/hcmuaf/fit/service/UserService.java
+++ src/main/java/vn/edu/hcmuaf/fit/service/UserService.java
@@ -6,8 +6,13 @@ import vn.edu.hcmuaf.fit.model.User;
 
 public class UserService {
     private static final UserService instance = new UserService();
-    public static UserService getInstance() { return instance; }
-    private UserService() {}
+
+    public static UserService getInstance() {
+        return instance;
+    }
+
+    private UserService() {
+    }
 
     public User checkLogin(String username, String password) {
         UserDAO dao = new UserDAO();
@@ -20,8 +25,31 @@ public class UserService {
         return matched ? user : null;
     }
 
+    public boolean checkUserExist(String username) {
+        return new UserDAO().checkUserExist(username);
+    }
+
+    public boolean checkEmailExist(String email) {
+        return new UserDAO().checkEmailExist(email);
+    }
+
     public void register(String username, String password, String email) {
         new UserDAO().register(username, hashPassword(password), email);
+    }
+
+    public boolean changePassword(int accountID, String oldPassword, String newPassword) {
+        UserDAO dao = new UserDAO();
+        String currentHashInDB = dao.getPasswordById(accountID);
+
+        if (currentHashInDB == null) return false;
+
+        boolean oldPasswordMatches = BCrypt.verifyer()
+                .verify(oldPassword.toCharArray(), currentHashInDB)
+                .verified;
+
+        if (!oldPasswordMatches) return false;
+
+        return dao.changePassword(accountID, hashPassword(newPassword));
     }
 
     public String hashPassword(String password) {
